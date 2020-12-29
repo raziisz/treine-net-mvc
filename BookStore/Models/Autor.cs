d312ef27fc57147a606526b2e093e5e40a282590
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-12-29 14:19:25 -0400

    add annotions and migrations

--- BookStore/Models/Autor.cs
+++ BookStore/Models/Autor.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class Autor
     {
+        public Autor()
+        {
+            this.Livros = new List<Livro>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo inválido")]
+        public string Nome { get; set; }
+
+        public ICollection<Livro> Livros { get; set; }
     }
 }
